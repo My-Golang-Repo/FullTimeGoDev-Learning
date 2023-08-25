348e5e5716121577fee63ddaf9a213a799a1dfa0
Suryana <nanasuryana335@gmail.com>
2023-08-25 21:27:46 +0700

    feat: complete initial go-fiber test

--- fulltimegodev-project/hotel-reservation/main.go
+++ fulltimegodev-project/hotel-reservation/main.go
@@ -8,14 +8,12 @@ import (
 
 func main() {
 	listenAddr := flag.String("listenAddr", ":5000", "The listen Address of API server")
+	flag.Parse()
+
 	app := fiber.New()
 	apiv1 := app.Group("/api/v1")
 
 	apiv1.Get("/user", api.HandleGetUsers)
 	apiv1.Get("/user/:id", api.HandleGetUser)
 	app.Listen(*listenAddr)
-}
-
-func handleUser(c *fiber.Ctx) error {
-	return c.JSON(map[string]string{"user": "Andromeda"})
 }
