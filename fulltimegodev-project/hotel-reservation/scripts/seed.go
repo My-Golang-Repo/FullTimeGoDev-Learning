3334ee82e4cb1f49575c4371350206da6efe5079
Suryana <nanasuryana335@gmail.com>
2023-08-28 20:41:18 +0700

    refactor: refactor main code to create it modular into functions

--- fulltimegodev-project/hotel-reservation/scripts/seed.go
+++ fulltimegodev-project/hotel-reservation/scripts/seed.go
@@ -5,28 +5,58 @@ import (
 	"fmt"
 	"github.com/fulltimegodev/hotel-reservation-nana/db"
 	"github.com/fulltimegodev/hotel-reservation-nana/types"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 )
 
+var (
+	roomStore  db.RoomStore
+	hotelStore db.HotelStore
+	ctx        = context.Background()
+)
+
 func main() {
-	ctx := context.Background()
+	seedHotel("UNCF Hotel", "Japan")
+	seedHotel("Yamato Hotel", "Iscandar")
+	seedHotel("Andromeda", "Saturn")
+}
+
+func init() {
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(db.DBURI))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	hotelStore := db.NewMongoHotelStore(client, db.DBNAME)
-	roomStore := db.NewMongoRoomStore(client, db.DBNAME)
-
-	hotel := types.Hotel{
-		Name:    "Saturn",
-		Address: "Singapore",
+	if err := client.Database(db.DBNAME).Drop(ctx); err != nil {
+		log.Fatal(err)
 	}
-	room := types.Room{
-		Type:      types.SingleRoomType,
-		BasePrice: 99.9,
+
+	hotelStore = db.NewMongoHotelStore(client)
+	roomStore = db.NewMongoRoomStore(client, hotelStore)
+}
+
+func seedHotel(name, address string) {
+	hotel := types.Hotel{
+		Name:    name,
+		Address: address,
+		Rooms:   []primitive.ObjectID{},
+	}
+
+	rooms := []types.Room{
+		{
+			Type:      types.SingleRoomType,
+			BasePrice: 99.9,
+		},
+		{
+			Type:      types.DoubleRoomType,
+			BasePrice: 129.9,
+		},
+		{
+			Type:      types.DeluxeRoomType,
+			BasePrice: 399.9,
+		},
 	}
 
 	insertedHotel, err := hotelStore.InsertHotel(ctx, &hotel)
@@ -34,13 +64,12 @@ func main() {
 		log.Fatal(err)
 	}
 
-	insertedRoom, err := roomStore.InsertRoom(ctx, &room)
-	if err != nil {
-		log.Fatal(err)
+	for _, room := range rooms {
+		room.HotelID = insertedHotel.ID
+		insertedRoom, err := roomStore.InsertRoom(ctx, &room)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(insertedRoom)
 	}
-
-	room.ID = insertedHotel.ID
-
-	fmt.Println(insertedHotel)
-	fmt.Println(insertedRoom)
 }
