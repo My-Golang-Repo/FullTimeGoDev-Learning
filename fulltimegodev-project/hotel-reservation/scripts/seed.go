276f37bc851bd024c92e02e6cd07c1b692a4a3da
Suryana <nanasuryana335@gmail.com>
2023-08-28 16:09:54 +0700

    feat: create seed database file

---
+++ fulltimegodev-project/hotel-reservation/scripts/seed.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/fulltimegodev/hotel-reservation-nana/db"
+	"github.com/fulltimegodev/hotel-reservation-nana/types"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+)
+
+func main() {
+	ctx := context.Background()
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(db.DBURI))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	hotelStore := db.NewMongoHotelStore(client, db.DBNAME)
+
+	hotel := types.Hotel{
+		Name:    "Saturn",
+		Address: "Singapore",
+	}
+	room := types.Room{
+		Type:      types.SingleRoomType,
+		BasePrice: 99.9,
+	}
+
+	insertedHotel, err := hotelStore.InsertHotel(ctx, &hotel)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	room.ID = insertedHotel.ID
+
+	fmt.Println(insertedHotel)
+}
