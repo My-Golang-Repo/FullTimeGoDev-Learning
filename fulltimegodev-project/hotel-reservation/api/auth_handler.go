7a68311e2b342d4aeacfbd0120fb3b98d37d8c0b
Suryana <nanasuryana335@gmail.com>
2023-09-02 19:12:08 +0700

    feat: create genericResp type and invalidCredentials function

--- fulltimegodev-project/hotel-reservation/api/auth_handler.go
+++ fulltimegodev-project/hotel-reservation/api/auth_handler.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v5"
 	"go.mongodb.org/mongo-driver/mongo"
+	"net/http"
 	"os"
 	"time"
 )
@@ -32,6 +33,18 @@ type AuthResponse struct {
 	Token string      `json:"token"`
 }
 
+type genericResp struct {
+	Type    string `json:"type"`
+	Message string `json:"message"`
+}
+
+func invalidCredentials(c *fiber.Ctx) error {
+	return c.Status(http.StatusBadRequest).JSON(genericResp{
+		Type:    "error",
+		Message: "invalid credentials",
+	})
+}
+
 func (h *AuthHandler) HandleAuthenticate(c *fiber.Ctx) error {
 	var params AuthParams
 	if err := c.BodyParser(&params); err != nil {
@@ -41,12 +54,12 @@ func (h *AuthHandler) HandleAuthenticate(c *fiber.Ctx) error {
 	user, err := h.userStore.GetUserByEmail(c.Context(), params.Email)
 	if err != nil {
 		if errors.Is(err, mongo.ErrNoDocuments) {
-			return fmt.Errorf("invalid credentials")
+			return invalidCredentials(c)
 		}
 	}
 
 	if !types.IsValidPassword(user.EncryptedPassword, params.Password) {
-		return fmt.Errorf("invalid credentials")
+		return invalidCredentials(c)
 	}
 
 	resp := AuthResponse{
