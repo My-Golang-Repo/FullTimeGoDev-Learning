e72fbdd45f3cd80f971de067a53a1b4d2e2529c8
Suryana <nanasuryana335@gmail.com>
2023-09-06 22:54:54 +0700

    feat: create new store, booking_store

--- fulltimegodev-project/hotel-reservation/db/booking_store.go
+++ fulltimegodev-project/hotel-reservation/db/booking_store.go
@@ -1,1 +1,48 @@
 package db
+
+import (
+	"context"
+	"github.com/fulltimegodev/hotel-reservation-nana/types"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type BookingStore interface {
+	InsertBooking(context.Context, *types.Booking) (*types.Booking, error)
+	GetBookings(context.Context, bson.M) ([]*types.Booking, error)
+}
+
+type MongoBookingStore struct {
+	client *mongo.Client
+	coll   *mongo.Collection
+}
+
+func NewMongoBookingStore(client *mongo.Client) *MongoBookingStore {
+	return &MongoBookingStore{
+		client: client,
+		coll:   client.Database(DBNAME).Collection("bookings"),
+	}
+}
+
+func (s *MongoBookingStore) InsertBooking(ctx context.Context, booking *types.Booking) (*types.Booking, error) {
+	resp, err := s.coll.InsertOne(ctx, booking)
+	if err != nil {
+		return nil, err
+	}
+
+	booking.ID = resp.InsertedID.(primitive.ObjectID)
+	return booking, nil
+}
+
+func (s *MongoBookingStore) GetBookings(ctx context.Context, filter bson.M) ([]*types.Booking, error) {
+	curr, err := s.coll.Find(ctx, filter)
+	if err != nil {
+		return nil, err
+	}
+	var books []*types.Booking
+	if err := curr.All(ctx, &books); err != nil {
+		return nil, err
+	}
+	return books, nil
+}
