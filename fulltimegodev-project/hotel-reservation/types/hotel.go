9a4453f04acbca03ba168b7b248515df9a937ee9
Suryana <nanasuryana335@gmail.com>
2023-09-09 12:50:43 +0700

    feat: add Seaside property to room type

--- fulltimegodev-project/hotel-reservation/types/hotel.go
+++ fulltimegodev-project/hotel-reservation/types/hotel.go
@@ -10,11 +10,10 @@ type Hotel struct {
 	Rating  int                  `bson:"rating" json:"rating"`
 }
 
-type RoomType int
-
 type Room struct {
 	ID      primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
 	Size    string             `bson:"size" json:"size"`
+	Seaside bool               `bson:"seaside" json:"seaside"`
 	Price   float64            `bson:"price" json:"price"`
 	HotelID primitive.ObjectID `bson:"hotelID" json:"hotelID"`
 }
