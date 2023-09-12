21e835d18c0438efa0e738315fcfe63abd5cc88b
Suryana <nanasuryana335@gmail.com>
2023-09-13 05:06:15 +0700

    refactor: refactor return err to use custom handling error

--- fulltimegodev-project/hotel-reservation/api/hotel_handler.go
+++ fulltimegodev-project/hotel-reservation/api/hotel_handler.go
@@ -20,20 +20,16 @@ func NewHotelHandler(store *db.Store) *HotelHandler {
 func (h *HotelHandler) HandleGetHotels(c *fiber.Ctx) error {
 	hotels, err := h.store.Hotel.GetHotels(c.Context(), nil)
 	if err != nil {
-		return err
+		return ErrResourceNotFound("Hotels")
 	}
 	return c.JSON(hotels)
 }
 
 func (h *HotelHandler) HandleGetHotel(c *fiber.Ctx) error {
 	id := c.Params("id")
-	oid, err := primitive.ObjectIDFromHex(id)
+	hotel, err := h.store.Hotel.GetHotelByID(c.Context(), id)
 	if err != nil {
-		return err
-	}
-	hotel, err := h.store.Hotel.GetHotelByID(c.Context(), oid)
-	if err != nil {
-		return err
+		return ErrResourceNotFound("Hotel")
 	}
 	return c.JSON(hotel)
 }
@@ -42,12 +38,12 @@ func (h *HotelHandler) HandleGetHotelRooms(c *fiber.Ctx) error {
 	id := c.Params("id")
 	oid, err := primitive.ObjectIDFromHex(id)
 	if err != nil {
-		return err
+		return ErrInvalidID()
 	}
 	filter := bson.M{"hotelID": oid}
 	rooms, err := h.store.Room.GetRooms(c.Context(), filter)
 	if err != nil {
-		return err
+		return ErrResourceNotFound("Rooms")
 	}
 	return c.JSON(rooms)
 }
