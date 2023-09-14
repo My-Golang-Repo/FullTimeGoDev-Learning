c393134ec64227cb8c03c88b81511c92c6d0fcdf
Suryana <nanasuryana335@gmail.com>
2023-09-14 22:17:13 +0700

    feat: create pagination and MongoDBEnvName

--- fulltimegodev-project/hotel-reservation/db/db.go
+++ fulltimegodev-project/hotel-reservation/db/db.go
@@ -1,10 +1,11 @@
 package db
 
-const (
-	DBNAME     = "hotel-reservation"
-	TestDBNAME = "hotel-reservation-test"
-	DBURI      = "mongodb://localhost:27017"
-)
+const MongoDBNameEnvName = "MONGO_DB_NAME"
+
+type Pagination struct {
+	Page  int64
+	Limit int64
+}
 
 type Store struct {
 	User    UserStore
