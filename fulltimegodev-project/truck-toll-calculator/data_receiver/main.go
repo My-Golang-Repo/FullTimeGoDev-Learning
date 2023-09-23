e3d86b25893906f256ec2755bbc5d9c931fb408f
Suryana <nanasuryana335@gmail.com>
2023-09-23 15:10:35 +0700

    feat: create kafka producer and add kafka producer to DataReceiver type

--- fulltimegodev-project/truck-toll-calculator/data_receiver/main.go
+++ fulltimegodev-project/truck-toll-calculator/data_receiver/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 	"github.com/PorcoGalliard/truck-toll-calculator/types"
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
 )
+
+const kafkaTopic = "obudata"
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
@@ -16,6 +19,7 @@ var upgrader = websocket.Upgrader{
 type DataReceiver struct {
 	msgch chan types.OBUdata
 	conn  *websocket.Conn
+	prod  *kafka.Producer
 }
 
 func NewDataReceiver() *DataReceiver {
@@ -25,6 +29,40 @@ func NewDataReceiver() *DataReceiver {
 }
 
 func main() {
+
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost"})
+	if err != nil {
+		panic(err)
+	}
+
+	defer p.Close()
+
+	// Delivery report handler for produced messages
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
+				} else {
+					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
+				}
+			}
+		}
+	}()
+
+	// Produce messages to topic (asynchronously)
+	topic := kafkaTopic
+	for _, word := range []string{"Welcome", "to", "the", "Confluent", "Kafka", "Golang", "client"} {
+		p.Produce(&kafka.Message{
+			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+			Value:          []byte(word),
+		}, nil)
+	}
+
+	// Wait for message deliveries before shutting down
+	p.Flush(15 * 1000)
+
 	recv := NewDataReceiver()
 	http.HandleFunc("/ws", recv.handleWS)
 	http.ListenAndServe(":30000", nil)
