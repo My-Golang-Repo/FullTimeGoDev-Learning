30ea0ba7db15258743490422bde32c611958f4b9
Suryana <nanasuryana335@gmail.com>
2023-10-01 23:16:01 +0700

    feat: create new type KafkaConsumer and message subscriber

--- fulltimegodev-project/truck-toll-calculator/distance_calculator/consumer.go
+++ fulltimegodev-project/truck-toll-calculator/distance_calculator/consumer.go
@@ -1,7 +1,51 @@
 package main
 
-import "github.com/PorcoGalliard/truck-toll-calculator/types"
+import (
+	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+	"github.com/sirupsen/logrus"
+	"time"
+)
 
-type DataConsumer interface {
-	ConsumeData(data types.OBUdata) error
+// This can also be called Kafka Transport
+type KafkaConsumer struct {
+	consumer  *kafka.Consumer
+	isRunning bool
+}
+
+func NewKafkaConsumer(topic string) (*KafkaConsumer, error) {
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost",
+		"group.id":          "myGroup",
+		"auto.offset.reset": "earliest",
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	err = c.SubscribeTopics([]string{topic}, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return &KafkaConsumer{
+		consumer: c,
+	}, nil
+}
+
+func (c *KafkaConsumer) Start() {
+	logrus.Info("kafka transport started")
+	c.isRunning = true
+	c.readMessageLoop()
+}
+
+func (c *KafkaConsumer) readMessageLoop() {
+	for c.isRunning {
+		msg, err := c.consumer.ReadMessage(time.Second)
+		if err != nil {
+			logrus.Errorf("Kafka consume error %s", err)
+			continue
+		}
+		fmt.Println(msg)
+	}
 }
