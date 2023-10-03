83e897c4dcbe29b2fa88d2e01ee98fd5e9a0d71f
Suryana <nanasuryana335@gmail.com>
2023-10-04 01:06:16 +0700

    feat(types): create a new type, Distance

--- fulltimegodev-project/truck-toll-calculator/types/types.go
+++ fulltimegodev-project/truck-toll-calculator/types/types.go
@@ -1,5 +1,10 @@
 package types
 
+type Distance struct {
+	Value float64 `json:"value"`
+	OBUID int     `json:"obuID"`
+}
+
 type OBUdata struct {
 	OBUID int     `json:"obuID"`
 	Lat   float64 `json:"lat"`
