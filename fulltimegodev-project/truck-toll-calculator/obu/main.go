755d688b9e3d324b3efacf5b640f8523f56c570e
Suryana <nanasuryana335@gmail.com>
2023-10-04 01:13:00 +0700

    feat(main): add LoggingMiddleware to obu main file

--- fulltimegodev-project/truck-toll-calculator/obu/main.go
+++ fulltimegodev-project/truck-toll-calculator/obu/main.go
@@ -11,7 +11,7 @@ import (
 
 const wsEndpoint = "ws://127.0.0.1:30000/ws"
 
-var sendInterval = time.Second
+var sendInterval = time.Second * 5
 
 func generateCoordinate() float64 {
 	n := float64(rand.Intn(100) + 1)
