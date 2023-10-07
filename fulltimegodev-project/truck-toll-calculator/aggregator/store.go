5199f479e98552ac6936de222f362ea579b9d550
Suryana <nanasuryana335@gmail.com>
2023-10-07 23:39:23 +0700

    feat: create new embedded function NewInvoiceGenerator

--- fulltimegodev-project/truck-toll-calculator/aggregator/store.go
+++ fulltimegodev-project/truck-toll-calculator/aggregator/store.go
@@ -3,12 +3,16 @@ package main
 import "github.com/PorcoGalliard/truck-toll-calculator/types"
 
 type MemoryStore struct {
+	data map[int]float64
 }
 
 func (m *MemoryStore) Insert(d types.Distance) error {
+	m.data[d.OBUID] += d.Value
 	return nil
 }
 
 func NewMemoryStore() *MemoryStore {
-	return nil
+	return &MemoryStore{
+		data: make(map[int]float64),
+	}
 }
