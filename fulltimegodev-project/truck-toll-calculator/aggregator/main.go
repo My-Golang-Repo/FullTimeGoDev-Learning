e4b3acb7c29f3d3f453e4ad6cb6c17928252966e
Suryana <nanasuryana335@gmail.com>
2023-10-11 04:32:20 +0700

    feat(main): add logging to main and WriteJSON function

--- fulltimegodev-project/truck-toll-calculator/aggregator/main.go
+++ fulltimegodev-project/truck-toll-calculator/aggregator/main.go
@@ -12,11 +12,12 @@ func main() {
 
 	listenAddr := flag.String("listenaddr", ":3000", "the listen address of HTTP Server")
 	flag.Parse()
-	store := NewMemoryStore()
 
 	var (
-		svc = NewInvoiceAggregator(store)
+		store = NewMemoryStore()
+		svc   = NewInvoiceAggregator(store)
 	)
+	svc = NewLogMiddleware(svc)
 	makeHTTPTransport(*listenAddr, svc)
 }
 
@@ -30,8 +31,18 @@ func handleAggregate(svc Aggregator) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var distance types.Distance
 		if err := json.NewDecoder(r.Body).Decode(&distance); err != nil {
-			w.WriteHeader(http.StatusBadRequest)
+			WriteJSON(w, http.StatusBadRequest, map[string]string{"error": err.Error()})
+			return
+		}
+		if err := svc.AggregateDistance(distance); err != nil {
+			WriteJSON(w, http.StatusInternalServerError, map[string]string{"error": err.Error()})
 			return
 		}
 	}
+}
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Add("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
 }
