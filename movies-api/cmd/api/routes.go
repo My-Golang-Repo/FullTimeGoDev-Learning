435fedfe7c1623d666dc5def086b573c335ecadd
Suryana <nanasuryana335@gmail.com>
2023-10-27 23:57:00 +0700

    feat(routes.go): add response while no route are found while requesting

--- movies-api/cmd/api/routes.go
+++ movies-api/cmd/api/routes.go
@@ -8,6 +8,9 @@ import (
 func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
 
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthCheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieDetailsHandler)
